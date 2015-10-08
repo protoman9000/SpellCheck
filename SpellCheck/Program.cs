a8e95ccf0d0e65fd2b13286ddb69dd74f250d43e
protoman9000 <oyekan900@yahoo.com>
2015-10-07 21:59:43 -0700

    going to test now, added some comments

--- SpellCheck/Program.cs
+++ SpellCheck/Program.cs
@@ -16,6 +16,7 @@ namespace SpellCheck
             int number = Convert.ToInt16(entry);
             string[] words = new string[number];
 
+            //Entering in the words we want to check.
             foreach (string word in words)
             {
                 int x = 1;
@@ -25,18 +26,27 @@ namespace SpellCheck
                 x++;
             }
             
+            //the plan is to check the words, character by character until we get a mismatch
             foreach (string word2 in words)
             {
                 string line;
                 StreamReader r = File.OpenText(@"C:\Users\Aziz\Downloads\Dict.txt");
                 while ((line = r.ReadLine()) != null)
-                {
-                    
+                {                   
                     for(int set = 0; set <= word2.Length; set++)
                     {
                         if (word2.Substring(set).Contains(line))
                         {
-
+                            continue;
+                        }
+                        else
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            sb.Append(word2);
+                            sb.Insert(set, ">");
+                            string tmp = word2;
+                            tmp = sb.ToString();
+                            Console.WriteLine(tmp);
                         }
                     }
                 }
